62f5e007c07724adbc66016b51656c79f0ade340
SupJoN <2633298327@qq.com>
2022-06-24 10:37:40 +0800

    v0.2

--- main/main.py
+++ main/main.py
@@ -1,4 +1,7 @@
 # coding:utf-8
+from Python.库.sort import quickly
+
+
 def Print():
     FxxkPy = r'''
 .------..------..------..------..------..------.
@@ -13,8 +16,25 @@ def Print():
 class _Main(object):
     def __init__(self):
         pass
-    def test(self):
+
+    def _test(self):
         print('一切正常')
+
+    # 快速排序
+    def _quickly(self, data: list) -> list:
+        original = [] + data  # 不改变原数据
+        if len(original) > 1:
+            standard = original[0]
+            del original[0]
+            left, right = [], []
+            for i in original:
+                if i >= standard:
+                    right.append(i)
+                else:
+                    left.append(i)
+            return self._quickly(left) + [standard] + self._quickly(right)
+        else:
+            return original
 
 
 Print()
@@ -22,3 +42,4 @@ Print()
 
 _main = _Main()
 test = _main.test
+quickly = _main._quickly
