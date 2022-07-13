aef504fcfec21cc6a2c2749a220318c9796de95d
John7457 <2633298327@qq.com>
2022-07-13 14:14:40 +0800

    0.1.0

--- fxxkpy/fxxkpy.py
+++ fxxkpy/fxxkpy.py
@@ -3,7 +3,7 @@ import sympy
 
 
 def __Print():
-    FxxkPy = '''\
+    FxxkPy = '''
 .------..------..------..------..------..------.
 |F.--. ||X.--. ||X.--. ||K.--. ||P.--. ||Y.--. |
 | :(): || :/\: || :/\: || :/\: || :/\: || (\/) |
@@ -14,76 +14,67 @@ def __Print():
     print(FxxkPy)
 
 
-class _Main(object):
-    # 初始化方法
-    def __init__(self, version):
-        self.version = version
-        self._cpp = _Cpp()
-        self._c = _C()
-        self._vector = _Vector
+# 快速排序
+def quickly(data: list) -> list:
+    original = [] + data  # 不改变原数据
+    if len(original) > 1:
+        standard = original[0]
+        del original[0]
+        left, right = [], []
+        for i in original:
+            if i >= standard:
+                right.append(i)
+            else:
+                left.append(i)
+        return quickly(left) + [standard] + quickly(right)
+    else:
+        return original
 
-    # 测试方法
-    def _test(self):
-        print(self.version + '  一切正常')
 
-    # 快速排序
-    def _quickly(self, data: list) -> list:
-        original = [] + data  # 不改变原数据
-        if len(original) > 1:
-            standard = original[0]
-            del original[0]
-            left, right = [], []
-            for i in original:
-                if i >= standard:
-                    right.append(i)
-                else:
-                    left.append(i)
-            return self._quickly(left) + [standard] + self._quickly(right)
-        else:
-            return original
+# 带参数的快速排序
+def pquickly(data: list, parameter: int) -> list:
+    original = [] + data  # 不改变原数据
+    if len(original) > 1:
+        standard = original[0]
+        del original[0]
+        left, right = [], []
+        for i in original:
+            if i[parameter] >= standard[parameter]:
+                right.append(i)
+            else:
+                left.append(i)
+        return pquickly(left) + [standard] + pquickly(right)
+    else:
+        return original
 
-    # 带参数的快速排序
-    def _pquickly(self, data: list, parameter: int) -> list:
-        original = [] + data  # 不改变原数据
-        if len(original) > 1:
-            standard = original[0]
-            del original[0]
-            left, right = [], []
-            for i in original:
-                if i[parameter] >= standard[parameter]:
-                    right.append(i)
-                else:
-                    left.append(i)
-            return self._pquickly(left) + [standard] + self._pquickly(right)
-        else:
-            return original
 
-    # 计算器
-    def _calculator(self, input_text: str) -> int:
-        text = list(input_text)
-        finally_ = ''
-        for i in range(len(text)):
-            if input_text[i] == '^':
-                text[i] = '**'
-        for i in text:
-            finally_ += i
-        return eval(finally_)
+# 计算器
+def calculator(input_text: str) -> int:
+    text = list(input_text)
+    finally_ = ''
+    for i in range(len(text)):
+        if input_text[i] == '^':
+            text[i] = '**'
+    for i in text:
+        finally_ += i
+    return eval(finally_)
 
-    # 斐波那契数列
-    def _fibonacci_sequence(self, index: int) -> int:
-        return int(sympy.S('1 / (5 ** 0.5) * (((1 + 5 ** 0.5) / 2) ** ' + str(index) + ' - ((1 - 5 ** 0.5) / 2) ** ' + str(index) + ')'))
 
-    # 质数判断
-    def _prime(self, num: int) -> bool:
-        if num > 1:
-            prime = True
-            for i in range(2, int(sympy.S(str(num) + ' ** (1 / 2)') + 1)):
-                if num % i == 0:
-                    prime = False
-            return prime
-        else:
-            return False
+# 斐波那契数列
+def fibonacci_sequence(index: int) -> int:
+    return int(sympy.S('1 / (5 ** 0.5) * (((1 + 5 ** 0.5) / 2) ** ' + str(index) + ' - ((1 - 5 ** 0.5) / 2) ** ' + str(index) + ')'))
 
+
+# 质数判断
+def prime(num: int) -> bool:
+    if num > 1:
+        prime = True
+        for i in range(2, int(sympy.S(str(num) + ' ** (1 / 2)') + 1)):
+            if num % i == 0:
+                prime = False
+        return prime
+    else:
+        return False
 
 # c++类
 class _Cpp(object):
@@ -107,13 +98,13 @@ class _Cout(object):
 class _C(object):
     def __init__(self):
         pass
-    
+
     def printf(self, content):
-        print(content, end = '')
+        print(content, end='')
 
 
 # 三维坐标
-class _Vector(object):
+class Vector3(object):
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
@@ -121,7 +112,7 @@ class _Vector(object):
 
     def __add__(self, other):
         if type(other) == self.__class__:
-            return _Vector(self.x + other.x,
+            return Vector3(self.x + other.x,
                            self.y + other.y,
                            self.z + other.z)
         else:
@@ -136,7 +127,7 @@ class _Vector(object):
 
     def __radd__(self, other):
         if type(other) == self.__class__:
-            return _Vector(self.x + other.x,
+            return Vector3(self.x + other.x,
                            self.y + other.y,
                            self.z + other.z)
         else:
@@ -151,7 +142,7 @@ class _Vector(object):
 
     def __iadd__(self, other):
         if type(other) == self.__class__:
-            return _Vector(self.x + other.x,
+            return Vector3(self.x + other.x,
                            self.y + other.y,
                            self.z + other.z)
         else:
@@ -166,7 +157,7 @@ class _Vector(object):
 
     def __mul__(self, other):
         if type(other) == int:
-            return _Vector(self.x * other,
+            return Vector3(self.x * other,
                            self.y * other,
                            self.z * other)
         else:
@@ -181,7 +172,7 @@ class _Vector(object):
 
     def __rmul__(self, other):
         if type(other) == int:
-            return _Vector(self.x * other,
+            return Vector3(self.x * other,
                            self.y * other,
                            self.z * other)
         else:
@@ -196,7 +187,7 @@ class _Vector(object):
 
     def __imul__(self, other):
         if type(other) == int:
-            return _Vector(self.x * other,
+            return Vector3(self.x * other,
                            self.y * other,
                            self.z * other)
         else:
@@ -215,7 +206,7 @@ class _Vector(object):
         else:
             return False
 
-    def __ne__(self,other):
+    def __ne__(self, other):
         return not self.__eq__(other)
 
     def __str__(self):
@@ -223,17 +214,5 @@ class _Vector(object):
 
 
 __Print()
-
-
-_main = _Main('v0.3.0-beta')
-cpp = _main._cpp
-
-c = _main._c
-
-Vector = _main._vector
-test = _main._test
-quickly = _main._quickly
-pquickly = _main._pquickly
-calculator = _main._calculator
-fibonacci_sequence = _main._fibonacci_sequence
-prime = _main._prime
+c = _C()
+cpp = _Cpp()
