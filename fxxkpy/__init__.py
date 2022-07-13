91f093b42c8069bc7b2b04aba458e695cffc24dc
John7457 <2633298327@qq.com>
2022-07-13 22:09:19 +0800

    0.1.10

--- fxxkpy/__init__.py
+++ fxxkpy/__init__.py
@@ -1,2 +1,3 @@
 # coding:utf-8
 from .fxxkpy import *
+from .vector import *
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ URL = 'https://github.com/John7457/fxxkpy'
 EMAIL = 'supjon@supjon.eu.org'
 AUTHOR = 'SupJoN'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.9'
+VERSION = '0.1.10'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
