7935e534577406c6246a00df377c887df678fbbd
John7457 <2633298327@qq.com>
2022-07-13 22:03:24 +0800

    0.1.9

--- fxxkpy/__init__.py
+++ fxxkpy/__init__.py
@@ -1,12 +1,2 @@
 # coding:utf-8
-import main
-
-
-quickly = main.quickly
-pquickly = main.pquickly
-calculator = main.calculator
-fibonacci_sequence = main.fibonacci_sequence
-prime = main.prime
-c = main.c
-cpp = main.cpp
-Vector3 = main.Vector3
+from .fxxkpy import *
