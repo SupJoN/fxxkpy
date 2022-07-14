eb9c3aad408cec198731a8bbc04ddf0f93047563
John7457 <2633298327@qq.com>
2022-07-14 20:04:27 +0800

    1.0.0

--- fxxkpy/other_languages.py
+++ fxxkpy/other_languages.py
@@ -6,18 +6,15 @@ class C():
         print(content, end='')
 
 
+# c++类
 # cout类
 class _Cout():
-    def __init__(self) -> None:
-        pass
-
     # << 方法
     def __lshift__(self, other: str) -> "_Cout":
         print(other, end='')
         return self
 
 
-# c++类
 class Cpp():
     cout = _Cout()
     endl = '\n'
