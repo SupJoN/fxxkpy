19e984817b7f4f9758ca8173540f9522ff621a94
John7457 <2633298327@qq.com>
2022-07-15 10:46:53 +0800

    1.0.0

--- fxxkpy/vector.py
+++ fxxkpy/vector.py
@@ -5,7 +5,7 @@ import sympy as __sympy
 # 坐标方法及方法类
 class vector():
     # 两坐标距离
-    def _distance3(vector3: "Vector3", other_vector3: "Vector3"):
+    def distance3(vector3: "Vector3", other_vector3: "Vector3"):
         res = __sympy.S('((' + str(vector3.x) + ' - ' + str(other_vector3.x) + ') ** 2 + \
                         (' + str(vector3.y) + ' - ' + str(other_vector3.y) + ') ** 2 + \
                         (' + str(vector3.z) + ' - ' + str(other_vector3.z) + ') ** 2) ** 0.5')
