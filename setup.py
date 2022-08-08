d76d8df5945ad656ebe752df2252d1d7df41e94f
John7457 <2633298327@qq.com>
2022-08-09 07:59:11 +0800

    final

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ EMAIL = 'supjon@supjon.eu.org'
 AUTHOR = 'SupJoN'
 MAINTAINER = 'SupJoN'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '2.0.0'
+VERSION = '2.0.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
