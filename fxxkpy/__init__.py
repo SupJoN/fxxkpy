ac9235bcc9c3a9223c4c1e4e9e908c73113e0298
John7457 <2633298327@qq.com>
2022-08-09 14:34:50 +0800

    final

--- fxxkpy/__init__.py
+++ fxxkpy/__init__.py
@@ -2,8 +2,8 @@
 from .start import *
 from .math import *
 from .vector import *
-from .other_languages import C as c
-from .other_languages import Cpp as cpp
-from .other_languages import Java as java
+from .languages import C as c
+from .languages import Cpp as cpp
+from .languages import Java as java
 from .error import *
 from .wallpaper import Dynamic as DynamicWallpaper
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ EMAIL = 'supjon@supjon.eu.org'
 AUTHOR = 'SupJoN'
 MAINTAINER = 'SupJoN'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '2.0.1'
+VERSION = '2.0.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
